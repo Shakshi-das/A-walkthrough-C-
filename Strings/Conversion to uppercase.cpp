e33dcd584b9a29c5d105403f5b69a16e804d0eb5
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-01-01 23:57:20 +0530

    Create Conversion to uppercase.cpp
    Program in cpp to replace all the lower-case letters of a given string with the corresponding capital letters.

---
+++ Strings/Conversion to uppercase.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // Including input-output stream header file
+#include <algorithm> // Including algorithm header for transform function
+using namespace std; // Using the standard namespace
+
+int main() {
+    string text; // Declaring a string variable named 'text' to store user input
+
+    getline(cin, text); // Getting a line of text input from the user
+
+    // Using transform function to convert each character of the string to uppercase
+    transform(text.begin(), text.end(), text.begin(), ::toupper);
+
+    // Displaying the converted string in uppercase
+    cout << text << endl;
+
+    return 0; // Indicating successful completion of the program
+}
